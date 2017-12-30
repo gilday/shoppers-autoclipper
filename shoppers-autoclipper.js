bd0e7014d25078e408597ca4907489943b362878
Johnathan Gilday <me@johnathangilday.com>
2017-12-30 14:36:43 -0500

    Cannot get the login form to render, so use ajax request to login directly instead. Update clip function to match new code

--- shoppers-autoclipper.js
+++ shoppers-autoclipper.js
@@ -1,55 +1,49 @@
 // shoppers-autoclipper.js CasperJS script
 
-const casper = require('casper').create()
+const casper = require('casper').create({
+    logLevel: 'debug',
+    verbose: true
+})
 
 const username = casper.cli.get('username')
 const password = casper.cli.get('password')
 
-casper.start('https://www.shoppersfood.com/savings/coupons.html', function () {
-  casper.viewport(1440, 1080) // viewport must be larger than default accommodate the responsive design
+casper.start('https://www.shoppersfood.com/savings/coupons.html', function start () {
+  casper.viewport(1920, 1080) // viewport must be larger than default accommodate the responsive design
   if (!(username && password)) {
     this.echo('username and password required')
     this.exit(1)
   }
 })
 
-// OPEN LOGIN FORM
-casper.then(function () {
-  this.evaluate(function openLoginForm () {
-    SimpleSignup.app = new SimpleSignup.Signin()
-    SimpleSignup.app.init()
-  })
-})
-
-// FILL USERNAME AND PASSWORD
-casper.waitUntilVisible('#ss-login-form')
-casper.wait(8000, function () {
-  this.fillSelectors('#ss-login-form', {
-    '#ss-email-input': username,
-    '#ss-password-login': password
-  }, false)
-  this.evaluate(function clickLogin () {
-    $('button.ss-login-btn').click()
-  })
-})
-casper.waitWhileVisible('#ss-login-form', function () {
-}, onTimeout('login timeout'), 60000)
+// LOGIN
+casper.then(function onPageReady () {
+  this.evaluate(function login (username, password) {
+    $.post('https://www.shoppersfood.com/services/login', {
+      j_username: username,
+      j_password: password,
+      rememberMe: false,
+      banner_id: 12
+    }).then(function reload () { window.location.reload() })
+  }, username, password)
+}, onTimeout('login'))
+casper.wait(20000) // TODO how to tell when login complete
 
 // CLIP
-casper.waitWhileVisible('.content .coupon-loading', function () {
+casper.waitWhileVisible('.coupon-spinner', function onCouponsLoaded () {
+  casper.capture('screenshots/04-coupons-loaded.jpg')
   this.evaluate(function () {
-    var clip = SVUMaster.couponAddToCard
-    function getUnclippedCouponIDs () {
+    var clip = CouponHelper.couponAddToCard
+    function getUnclippedCoupons () {
       // requesting page number 0 returns all coupons in a single page
       return $.getJSON('https://www.shoppersfood.com/services/couponlist?category=All&currentPageNumber=0').then(function (data) {
         return data.coupons
           .filter(function (c) { return !c.onCard })
-          .map(function (c) { return c.id })
       })
     }
     function clipAll () {
-      return getUnclippedCouponIDs().then(function (ids) {
-        const queue = ids.map(function (id) { return clip.bind(null, id) })
+      return getUnclippedCoupons().then(function (coupons) {
+        const queue = coupons.map(function (coupon) { return clip.bind(null, coupon) })
         return queue.reduce(function (prev, cur) { return prev.then(cur) }, $.Deferred().resolve())
       })
     }
@@ -61,16 +55,19 @@ casper.waitWhileVisible('.content .coupon-loading', function () {
 }, onTimeout('loading coupons timeout'), 20000)
 
 casper.waitForSelector('#shoppers-autoclip-complete-marker', function () {
+  casper.capture('screenshots/05-coupons-clipped.jpg')
   this.echo('clipped all coupons')
-}, onTimeout('clipping coupons timeout'), 300000)
+}, onTimeout('clipping coupons timeout'), 360000)
 
 casper.run()
 
 function onTimeout (msg) {
-  if (msg == null) {
+  if (!msg) {
     msg = 'timeout waiting for page to load'
   }
   return function () {
+    this.echo(this.getHTML('.ss-container'))
+    casper.capture('screenshots/06-timeout.jpg')
     this.echo(msg).exit(1)
   }
 }
